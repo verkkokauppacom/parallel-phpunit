a0029c3432f78661d622e859890e70374a1b81a3
Samuli Siivonen <samuli.siivonen@reaktor.fi>
2012-12-20 08:07:45 +0200

    Add test for parallel-phpunit execution

---
+++ tests/ParallelPhpUnitTest.php
@@ -0,0 +1,28 @@
+<?php
+class ParallelPhpUnitTest extends PHPUnit_Framework_TestCase
+{
+    public function testHelpMessage()
+    {
+        $helpOutput = <<<EOS
+Running parallel wrapper for phpunit
+Paralleling options:
+    --pu-cmd - custom phpunit run script, default phpunit
+    --pu-thread - max threads, default 3
+    --pu-test-mask - tests file template, default *Test.php
+    --pu-verbose - show addional information (0|1), default 0
+Usage: parallel-phpunit [switches] <directory>
+EOS;
+        $this->verifyCommandOutput("", 1, $helpOutput);
+    }
+
+    private function verifyCommandOutput($arguments, $expectedExitStatus = 0, $expectedOutput = null)
+    {
+        $parallel_phpunit = __DIR__ . "/../bin/parallel-phpunit";
+        $output = array();
+        $exitStatus = -1;
+        exec($parallel_phpunit, $output, $exitStatus);
+        $this->assertEquals($expectedExitStatus, $exitStatus);
+        $expectedOutput && $this->assertEquals($expectedOutput, implode("\n", $output));
+    }
+}
+?>
