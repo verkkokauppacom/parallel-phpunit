79310596d5c46eb7728bf2f9d65161d1f14158ee
Samuli Siivonen <samuli.siivonen@reaktor.fi>
2013-01-15 07:48:42 +0200

    Add groups to example tests

--- example/1/ATest.php
+++ example/1/ATest.php
@@ -1,6 +1,9 @@
 <?php
 class ATest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @group a
+     */
     public function testIt()
     {
         sleep(2);
--- example/2/BTest.php
+++ example/2/BTest.php
@@ -1,6 +1,9 @@
 <?php
 class BTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @group b
+     */
     public function testIt()
     {
         sleep(1);
