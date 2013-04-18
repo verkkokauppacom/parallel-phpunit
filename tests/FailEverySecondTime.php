21efcb52d7a88d8b772f427958b4a963b0cf5468
Samuli Siivonen <samuli.siivonen@reaktor.fi>
2013-04-18 11:12:48 +0300

    Add test that fails every second time
    This will be used to test the --pu-retires functionality.

---
+++ tests/FailEverySecondTime.php
@@ -0,0 +1,15 @@
+<?php
+class FailEverySecondTime extends PHPUnit_Framework_TestCase
+{
+    public function test()
+    {
+        $file = "/tmp/failTheTest";
+        if(file_exists($file))
+        {
+            unlink($file);
+            $this->assertTrue(false);
+        } else {
+            file_put_contents($file, "");
+        }
+    }
+}
